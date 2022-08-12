d1c3b7c5f6fa562b0dcf5b479df95f4bce5e8a53
habibeshadow <habibe@kayfo.games>
2022-08-12 00:19:18 +0000

    New mprew strapi + postgre local

--- config/database.js
+++ config/database.js
@@ -1,13 +1,31 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'mprew_back_mysql'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', 'root'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
+
+  // strapi-api/config/database.js
+  module.exports = ({ env }) => ({
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'mprew_back_mysql'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'root'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'mprew'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'root'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+         
+      },
+      debug: false,
     },
-  },
-});
+  });
