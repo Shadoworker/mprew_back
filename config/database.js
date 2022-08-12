6fa6d581d8a0c7c128306da23595d056124cbea9
habibeshadow <habibe@kayfo.games>
2022-08-12 00:33:14 +0000

    New mprew strapi + heroku pgres creds

--- config/database.js
+++ config/database.js
@@ -18,11 +18,11 @@
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'ec2-52-207-15-147.compute-1.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'mprew'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'root'),
+        database: env('DATABASE_NAME', 'd5360hs5la7ui'),
+        user: env('DATABASE_USERNAME', 'fbjhxifjltliyc'),
+        password: env('DATABASE_PASSWORD', '5542fd179be40b270d337a5d4c9e197847c0927d14ea8206601bdab29e3a00f4'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not required
          
       },
